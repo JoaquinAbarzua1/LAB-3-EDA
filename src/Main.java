65dc3fb17ed9e80d495e8704d4c6da488052d351
Joaquin Abarzua <joaquin.abarzua@mail.udp.cl>
2025-05-27 16:56:46 -0400

    busqueda para cada metodo getGamesByX. Por ahora falta: - Métodos de ordenamiento para sortByAlgorithm - Clase GenerateData - Main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
@@ -34,228 +35,174 @@ class Game{
 
 class Dataset{
     private ArrayList<Game> data;
-    private String sortedByAtribute;
+    private String sortedByAttribute;
 
     public Dataset (ArrayList<Game> data){
         this.data = data;
-        this.sortedByAtribute = "unSorted";
+        this.sortedByAttribute = "notSorted";
     }
 
 
 
     public ArrayList<Game> getGamesByPrice(int price){
-       Arraylist<Game> resultado = newArraylist<>();   //creamos arreglo
-        if (this.sortedByAtribute.equals("price")){
-        int left = 0 ;  // auxiliar de inicio
-        int right = Data.size() - 1 ;  //auxiliar 2 de inicio
-            while (left <= right){      // condicion para crear la mitad 
-                int mid = (left + right ) / 2 ; 
-                int midPrice = data.get(mid).getPrice();  // valor del medio 
-                    if ( midPrice == price ) {
-                        int i = mid;
-                            while ( i >= 0 && data.get(i).getPrice() == price ){ //ver si hayy mas juegos para la izquierda con los mismos valores 
-                        result.add(0 , data.get(i));
-                        i --;
+        ArrayList<Game> resultado = new ArrayList<>();   //creamos arreglo
+        if (this.sortedByAttribute.equals("price")){ //busqueda binaria si está ordenado
+            int lo = 0 ;  // auxiliar de inicio
+            int hi = data.size() - 1 ;  //auxiliar 2 de inicio
+
+            while (lo <= hi){      // condicion para crear la mitad
+                int mid = (lo + hi ) / 2 ;
+
+                if ( data.get(mid).getPrice() == price ) { //Si el juego que está en la posición mid tiene el precio buscado
+                    int aux = mid;
+                    while (data.get(aux).getPrice() == price){ //ve si hay más juegos para la izquierda con los mismos valores
+                        resultado.add(data.get(aux)); //Saqué lo de addFirst o add(0,dato) porque es de complejidad lineal
+                        aux --;
                     }
-                     int i = mid + 1 ; 
-                        while( i < data.size() && data.get(i).getPrice() == price ){ //ver si hayy mas juegos para la derecha con los mismos valores 
-                            result.add( data.get(i));
-                            i ++;
-                        }  
-                        break; 
-                    }
-                else if ( midPirce < price ){
-                    left = mid + 1 ;
-                    
-                }
-                else {
-                    right = mid - 1;
-                }
-            } 
-        } 
-        else // lineal 
-            for(game g : data){
-                if(g.getPrice == price){
-                    result.add(g);
-                }
-            }  
-        return result;            
-    }
-       
-
-    public ArrayList<Game> getGamesByPriceRange(int lowerPrice, int higherPrice){
-    Arraylist<Game> resultado = newArraylist<>();
-         if (sortedByAtribute.equals("price")) {
-        // Búsqueda binaria para el límite inferior
-        int left = 0, right = data.size() - 1;
-        int start = -1;
-
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            int midPrice = data.get(mid).getPrice();
-
-            if (midPrice >= lowerPrice) {
-                start = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-
-        if (start != -1) {
-            for (int i = start; i < data.size(); i++) {
-                int price = data.get(i).getPrice();
-                if (price > higherPrice) break;
-                result.add(data.get(i));
-            }
-        }
-
-    } else {
-        for (Game g : data) {
-            int price = g.getPrice();
-            if (price >= lowerPrice && price <= higherPrice) {
-                result.add(g);
-            }
-        }
-    }
-
-    return result;
-}
-
-
-
-        
-    public ArrayList<Game> getGamesByCategory(String category){
-Arraylist<game> resultado = newArraylist<>();
-        if(thissortedByAtribute.equals("category")){
-            int left  = 0;
-                int right = data.size() - 1 ;
-            while(left <= right){
-                int mid = (left + right) / 2 ;
-                String midCategory = data.get(mid).getCategory(); 
-                int comparacion = midCategory.compareTo(category);
-                if( comparacion == 0 ) {
-                    int i = mid ;
-                    while( i >= data.get(i).getCategory().equals(category)){
-                        resultado.add(0 , data.get(i));
-                        i--;
-                }
-                    i = mid + 1;
-                    while( i < data.size() && data.get(i).getCategory().equals(category)){
-                    resultado.add(o , data.get(i))
-                    i++;    
+                    aux = mid + 1 ;
+                    while(data.get(aux).getPrice() == price){ //ve si hay mas juegos para la derecha con los mismos valores
+                        resultado.add( data.get(aux));
+                        aux ++;
                     }
                     break;
-        }
-                else if (comparacion < 0) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+                }
+
+                else if ( data.get(mid).getPrice() < price ){ lo = mid + 1 ; } //nos movemos a la derecha
+
+                else { hi = mid - 1; } //nos movemos a la izquierda
             }
         }
-            else{
-            for(game g : data){
-                if(g.getCategory().equals("category")){
+        else { // En caso de que el arreglo no esté ordenado por precio
+            for (Game g : data) {
+                if (g.getPrice() == price) {
                     resultado.add(g);
                 }
             }
-
-                
-    }
-      return resultado;
-    }
-
-
-
-
-
-        
-    public ArrayList<Game> getGamesByQuality(int quality){
-       Arraylist<Game> resultado = nesArraylist<>();
-        if(this.sortedByAtribute.equals("quality")){
-            int left = 0 ;
-            int right = data.size() - 1 ;
-            while(left < right ){
-                int mid = (left + right ) /2 ;
-                    String midQuality = data.get(mid).getQuality();
-                        int comparacion = midQuality.comparteTo(quality);
-                        if(comparacion == 0 ){
-                            int i = mid ;
-                        }
-            }
-                            while(i >= 0 && data.get(i).getQuality().equals(quality)){
-                               resultado.add(0 , data.get(i));
-                                i--;
-                            }
-                            i = mid + 1 ;
-                            while(i < data.size() && data.get(i).getQuality().equals(quality) ){
-                                resultado.add(0 , data.get(i));
-                                i++;
-                                
-                            }
-                               break; 
-                        
-                        }
-                          else if (comparacion < 0) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }    
-
-
-        else{
-           for(Game g : data){
-               if(g.getQuality().equals("quality"){
-                   resultado.add(g);
-        }
-               
-           }
         }
         return resultado;
     }
 
-    
-  public void sortByAlgorithm(String algorithm, String attribute) {
-    Comparator<Game> comparator;
 
-    // Seleccionar el atributo por el cual se ordenará
-    switch (attribute) {
-        case "category":
-            comparator = Comparator.comparing(Game::getCategory);
-            break;
-        case "quality":
-            comparator = Comparator.comparingInt(Game::getQuality);
-            break;
-        case "price":
-        default:
-            comparator = Comparator.comparingInt(Game::getPrice);
-            attribute = "price"; // por defecto
-            break;
+    public ArrayList<Game> getGamesByPriceRange(int lowerPrice, int higherPrice){
+        ArrayList<Game> resultado = new ArrayList<>();
+        if (this.sortedByAttribute.equals("price")){
+            int lo = 0;
+            int hi = data.size() - 1;
+
+            while (lo <= hi){
+                int mid = (lo + hi ) / 2 ;
+
+                if ((data.get(mid).getPrice() >= lowerPrice) && (data.get(mid).getPrice() <= higherPrice)){ //Si el precio del juego está dentro del rango, se agrega
+                    int aux = mid;
+                    while ((data.get(mid).getPrice() >= lowerPrice) && (data.get(mid).getPrice() <= higherPrice)){ //ve si hay más juegos para la izquierda dentro del rango
+                        resultado.add(data.get(aux)); //Saqué el addFirst porque es de complejidad lineal
+                        aux --;
+                    }
+                    aux = mid + 1 ;
+                    while((data.get(mid).getPrice() >= lowerPrice) && (data.get(mid).getPrice() <= higherPrice)){ //ve si hay mas juegos para la derecha dentro del rango
+                        resultado.add( data.get(aux));
+                        aux ++;
+                    }
+                    break;
+                }
+                else if ( data.get(mid).getPrice() < lowerPrice ){ lo = mid + 1 ; } //nos movemos a la derecha si estamos bajo el rango
+
+                else if ( data.get(mid).getPrice() >higherPrice ) { hi = mid - 1 ; } //nos movemos a izquierda si estamos sobre el rango
+            }
+        }
+        else { // busqueda lineal en caso de que el arreglo no esté ordenado por precio
+            for (Game g : data) {
+                if ((g.getPrice() >= lowerPrice) && (g.getPrice() <= higherPrice)) {
+                    resultado.add(g);
+                }
+            }
+        }
+        return resultado;
     }
 
-    // Ordenar usando el algoritmo especificado
-    switch (algorithm) {
-        case "bubbleSort":
-            bubbleSort(comparator);
-            break;
-        case "insertionSort":
-            insertionSort(comparator);
-            break;
-        case "selectionSort":
-            selectionSort(comparator);
-            break;
-        case "mergeSort":
-            data = mergeSort(data, comparator);
-            break;
-        case "quickSort":
-            quickSort(0, data.size() - 1, comparator);
-            break;
-        default:
-            Collections.sort(data, comparator);
-            break;
+    public ArrayList<Game> getGamesByCategory(String category){
+        ArrayList<Game> resultado = new ArrayList<>();
+        if (this.sortedByAttribute.equals("category")){ //busqueda binaria si está ordenado
+            int lo = 0 ;  // auxiliar de inicio
+            int hi = data.size() - 1 ;  //auxiliar 2 de inicio
+
+            while (lo <= hi){      // condicion para crear la mitad
+                int mid = (lo + hi ) / 2 ;
+                int comparacion = data.get(mid).getCategory().compareTo(category); //Compara la categoria del juego que estoy viendo con la categoría buscada, -1 si es menor, 0 si es la que busco, 1 si es mayor
+
+                if (comparacion == 0) { //Si la comparación me retorna 0, es porque el juego que estoy viendo es de la categoría buscada
+                    int aux = mid;
+                    while (data.get(aux).getCategory().equals(category)){ //ve si hay más juegos para la izquierda con la misma categoría
+                        resultado.add(data.get(aux));
+                        aux --;
+                    }
+                    aux = mid + 1 ;
+                    while(data.get(aux).getCategory().equals(category)){ //ve si hay mas juegos para la derecha con la misma categoría
+                        resultado.add( data.get(aux));
+                        aux ++;
+                    }
+                    break;
+                }
+
+                else if ( comparacion < 0){ lo = mid + 1 ; } //nos movemos a la derecha
+
+                else { hi = mid - 1; } //nos movemos a la izquierda
+            }
+        }
+        else { // En caso de que el arreglo no esté ordenado por categoría
+            for (Game g : data) {
+                if (g.getCategory().equals(category)) {
+                    resultado.add(g);
+                }
+            }
+        }
+        return resultado;
     }
 
-    this.sortedByAtribute = attribute;
+    public ArrayList<Game> getGamesByQuality(int quality){
+        ArrayList<Game> resultado = new ArrayList<>();
+        if (this.sortedByAttribute.equals("quality")){ //busqueda binaria si está ordenado por calidad
+            int lo = 0 ;  // auxiliar de inicio
+            int hi = data.size() - 1 ;  //auxiliar 2 de inicio
+
+            while (lo <= hi){      // condicion para crear la mitad
+                int mid = (lo + hi ) / 2 ;
+
+                if ( data.get(mid).getQuality() == quality ) { //Si el juego que está en la posición mid tiene la calidad buscada
+                    int aux = mid;
+                    while (data.get(aux).getQuality() == quality){ //ve si hay más juegos para la izquierda con la misma calidad
+                        resultado.add(data.get(aux));
+                        aux --;
+                    }
+                    aux = mid + 1 ;
+                    while(data.get(aux).getQuality() == quality){ //ve si hay mas juegos para la derecha con la misma calidad
+                        resultado.add( data.get(aux));
+                        aux ++;
+                    }
+                    break;
+                }
+
+                else if ( data.get(mid).getQuality() < quality ){ lo = mid + 1 ; } //nos movemos a la derecha
+
+                else { hi = mid - 1; } //nos movemos a la izquierda
+            }
+        }
+        else { // En caso de que el arreglo no esté ordenado por precio
+            for (Game g : data) {
+                if (g.getQuality() == quality) {
+                    resultado.add(g);
+                }
+            }
+        }
+        return resultado;
+    }
+
+    public void sortByAlgorithm(String algorithm, String attribute){
+        this.sortedByAttribute = attribute;
+        //se podría hacer de 2 formas, con "case : " o con if
+        //con "case : " sería más ordenado, porque se puede escribir cada algoritmo fuera de este metodo y de forma ordenada
+    }
+
 }
 //------------------------Clase GenerateData-----------------------
 class GenerateData{
